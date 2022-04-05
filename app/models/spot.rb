f31f998706062165627c677ad19f17ca5cf58bc1
Mickael Toledano gh api mikatol06@gmail.com <mikatol06@gmail.com>
2022-04-05 14:56:41 +0200

    mapbox with geolocate and markers

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,5 @@
 class Spot < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Spot.destroy_all
+
+puts "Creating spots..."
+spot1 = Spot.new(address: "57 rue de la Buffa, 06000 Nice", latitude: 43.6963, longitude: 7.2583, spot_number: 2)
+spot1.save!
+spot2 = Spot.new(address: "2 rue Nathalie Masse, 06000 Nice", latitude: 43.7115, longitude: 7.2825, spot_number: 1)
+spot2.save!
+spot3 = Spot.new(address: "5 rue Jean François Fulconis, 06000 Nice", latitude: 43.7234, longitude: 7.2857, spot_number: 2)
+spot3.save!
+puts "Finished!"
