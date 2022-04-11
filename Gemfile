9e0aaaf362f0a515e0a8011af118e0fd5132b3c7
Mickael Toledano gh api mikatol06@gmail.com <mikatol06@gmail.com>
2022-04-11 15:26:47 +0200

    fave

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'devise'
 gem "geocoder"
+gem "jquery-rails"
 
 
 gem 'autoprefixer-rails', '10.2.5'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_150403) do
+ActiveRecord::Schema.define(version: 2022_04_11_094623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "spot_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["spot_id"], name: "index_likes_on_spot_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "spots", force: :cascade do |t|
     t.float "longitude"
@@ -37,4 +46,6 @@ ActiveRecord::Schema.define(version: 2022_04_08_150403) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "spots"
+  add_foreign_key "likes", "users"
 end
