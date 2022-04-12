d07ced2092a23abb4ede9bd75913349898768e66
zied oussaifi <ousaifizied@hotmail.fr>
2022-04-12 12:12:13 +0200

    added street view

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -3,6 +3,7 @@ class SpotsController < ApplicationController
 
   def index
     @spots = Spot.all
+
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
     @markers = @spots.geocoded.map do |spot|
       {
@@ -16,38 +17,36 @@ class SpotsController < ApplicationController
 
   def show
     @spot = Spot.find(params[:id])
-    # @spot = spot
-    # @marker ={
-    #   lat: spot.latitude,
-    #   lng: spot.longitude,
-    #   info_window: render_to_string(partial: "info_window", locals: { spot: spot })
 
-    # }
     @spot_map = Spot.where(id: params[:id])
     @marker = @spot_map.geocoded.map do |spot|
-
       {
         lat: spot.latitude,
         lng: spot.longitude,
         info_window: render_to_string(partial: "info_window", locals: { spot: spot })
 
-
-
       }
+    end
+
+    @spot_map.each do |spot|
+      @url = "https://www.google.com/maps/dir/le+wagon+nice/#{spot.latitude}+#{spot.longitude}"
+    end
+
+    @spot_map.each do |spot|
+      @url2 = "http://maps.google.com/?cbll=#{spot.latitude},#{spot.longitude}&cbp=12,20.09,,0,5&layer=c"
     end
   end
 
   def matches
-    @spots = Spot.near(params[:search][:address], 0.2)
-    @markers = @spots.geocoded.map do |spot|
 
+    @spots = Spot.near(params[:search][:address], 0.2)
+
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @spots.geocoded.map do |spot|
       {
         lat: spot.latitude,
         lng: spot.longitude,
         info_window: render_to_string(partial: "info_window", locals: { spot: spot })
-
-
-
       }
     end
   end
