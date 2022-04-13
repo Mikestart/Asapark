bf348945fbba80c6f84ff8d312687dee06c7f459
Lila Atmani <sunbornone@gmail.com>
2022-04-13 16:41:42 +0200

    changed the front in maps, deleted mapbox trademark

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -25,7 +25,7 @@ class SpotsController < ApplicationController
         lat: spot.latitude,
         lng: spot.longitude,
         info_window: render_to_string(partial: "info_window", locals: { spot: spot }),
-        image_url: helpers.asset_url("icon_map.svg")
+        image_url: helpers.asset_url("spot_handicap_purple.svg")
       }
     end
     @spot_map.each do |spot|
@@ -46,7 +46,7 @@ class SpotsController < ApplicationController
         lat: spot.latitude,
         lng: spot.longitude,
         info_window: render_to_string(partial: "info_window", locals: { spot: spot }),
-        image_url: helpers.asset_url("icon_map.svg")
+        image_url: helpers.asset_url("spot_handicap_purple.svg")
       }
     end
   end
